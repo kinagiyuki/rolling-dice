7ff7914d56cd3f4843d3561f8462d8cd8d34a1ea
kinagiyuki <kinagiyuki@gmail.com>
2025-09-24 23:53:01 +0800

    feat: separate the menu functions into `menu` module

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 mod dice_generator;
+mod menu;
 
+use crate::menu::Menu;
 use clap::{Parser, Subcommand};
-use dice_generator::DiceGenerator;
-use regex::Regex;
 
 #[derive(Parser, Debug)]
 #[command(name = "rolling-dice", about = "A dice rolling program")]
@@ -21,27 +21,10 @@ enum Commands {
 
 fn main() {
     let args = Args::parse();
-    let mut dice_generator = DiceGenerator::new();
+    let mut menu = Menu::new();
 
     match args.command {
-        Commands::Roll { dice} => {
-            let re = Regex::new(r"^(\d+)[dD](\d+)$").unwrap();
-            let captures = re.captures(&dice).expect("Invalid rolling text");
-            let n: usize = captures[1].parse().expect("Invalid number of dice");
-            let m: u32 = captures[2].parse().expect("Invalid number of faces");
-
-            match dice_generator.generate(n, m) {
-                Ok(results) => println!("{}", results),
-                Err(e) => eprintln!("Error generating dice: {}", e),
-            }
-        },
-        Commands::History => match dice_generator.history() {
-            Ok(records) => {
-                for record in records {
-                    println!("{}", record)
-                }
-            },
-            Err(e) => eprintln!("Error: {}", e),
-        },
+        Commands::Roll { dice } => menu.roll(dice),
+        Commands::History => menu.history(),
     }
 }
---
+++ src/menu.rs
@@ -0,0 +1,39 @@
+use crate::dice_generator;
+
+use dice_generator::DiceGenerator;
+use regex::Regex;
+
+pub struct Menu {
+    generator: DiceGenerator,
+}
+
+impl Menu {
+    pub fn new() -> Self {
+        Menu {
+            generator: DiceGenerator::new(),
+        }
+    }
+
+    pub fn roll(&mut self, roll_str: String) {
+        let re = Regex::new(r"^(\d+)[dD](\d+)$").unwrap();
+        let captures = re.captures(&roll_str).expect("Invalid rolling text");
+        let n: usize = captures[1].parse().expect("Invalid number of dice");
+        let m: u32 = captures[2].parse().expect("Invalid number of faces");
+
+        match self.generator.generate(n, m) {
+            Ok(results) => println!("{}", results),
+            Err(e) => eprintln!("Error generating dice: {}", e),
+        }
+    }
+
+    pub fn history(&mut self) {
+        match self.generator.history(true) {
+            Ok(records) => {
+                for record in records {
+                    println!("{}", record)
+                }
+            },
+            Err(e) => eprintln!("Error: {}", e),
+        }
+    }
+}
